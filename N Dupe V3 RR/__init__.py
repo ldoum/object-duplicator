e0d5f96d138f0da9fb10f7e7f18dfc0bbd3d7842
Lancine Doumbia <107377652+ldoum@users.noreply.github.com>
2025-10-19 00:03:46 -0400

    Update bl_info version to 3.1.1

--- N Dupe V3 RR/__init__.py
+++ N Dupe V3 RR/__init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "blender": (2, 8, 0),
     "category": "Object",
     "author": "Lancine Doumbia", #maintainer
-    "version": (3, 0, 0), 
+    "version": (3, 1, 1), 
     "location": "View3D > Sidebar", #important
     "description": "Duplicate an object N times", #
     "warning": "",
@@ -26,4 +26,5 @@ def unregister():
     panels.unregister()
 
 if __name__ == "__main__":
+
     register()
