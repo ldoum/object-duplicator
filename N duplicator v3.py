bdd1a4a8ed0c679641808c72812ce151223db913
Lancine Doumbia <107377652+ldoum@users.noreply.github.com>
2025-10-19 00:03:03 -0400

    Implement reset operator for duplication settings
    Added reset functionality to duplication properties.

--- N duplicator v3.py
+++ N duplicator v3.py
@@ -57,8 +57,8 @@ class DuplicateObjectProperties(bpy.types.PropertyGroup):
         name = "X Direction",
         description = "Direction of copies on X axis",
         items = [
+            ('RIGHT', "Right", "Copies will be created to the right"),
             ('LEFT', "Left", "Copies will be created to the left"),
-            ('RIGHT', "Right", "Copies will be created to the right")
         ],
         default = 'RIGHT'
     )
@@ -68,7 +68,7 @@ class DuplicateObjectProperties(bpy.types.PropertyGroup):
         description = "Direction of copies on Y axis",
         items = [
             ('FORWARD', "Forward", "Copies will be created forward"),
-            ('BACKWARD', "Backward", "Copies will be created backward")
+            ('BACKWARD', "Backward", "Copies will be created backward"),
         ],
         default = 'FORWARD'
     )
@@ -78,7 +78,7 @@ class DuplicateObjectProperties(bpy.types.PropertyGroup):
         description = "Direction of copies on Z axis",
         items = [
             ('UP', "Up", "Copies will be created upward"),
-            ('DOWN', "Down", "Copies will be created downward")
+            ('DOWN', "Down", "Copies will be created downward"),
         ],
         default = 'UP'
     )
@@ -164,7 +164,28 @@ class OBJECT_OT_Toggle_Z_Mode(OperatorMixin, bpy.types.Operator):
         self.props.toggle_z_axis = not self.props.toggle_z_axis
         
         return {"FINISHED"}  
+
+class OBJECT_OT_Reset_All(OperatorMixin, bpy.types.Operator):
+    bl_idname = "my_operator.reset_dupe_data"
+    bl_label = "My Class Name"
+    bl_description = "Description that shows in blender tooltips"
+    
+    def execute(self, context):
         
+        self.props.copies_x_line = 1
+        self.props.distance_x_line = 1.00
+        self.props.direction_of_x_line_copies = "RIGHT"
+        
+        self.props.copies_y_line = 1
+        self.props.distance_y_line = 1.00
+        self.props.direction_of_y_line_copies = "FORWARD"
+        
+        self.props.copies_z_line = 1
+        self.props.distance_z_line = 1.00
+        self.props.direction_of_z_line_copies = "UP"
+        
+        return {"FINISHED"} 
+
 class OBJECT_OT_Duplicate_All(OperatorMixin, bpy.types.Operator):
     bl_idname = "my_operator.apply_duplication"
     bl_label = "My Class Name"
@@ -230,10 +251,8 @@ class OBJECT_PT_Duplicates_Panel(PanelMixin, bpy.types.Panel):
       
         #all in the box
         boxy = layout.box()
-        
-        column2 = boxy.column(align=True)
+        column1 = boxy.column(align=True)
        
-
         set_panel = [
             ("X", props.toggle_x_axis, "copies_x_line", "distance_x_line" ,"direction_of_x_line_copies" ),
             ("Y", props.toggle_y_axis, "copies_y_line", "distance_y_line" ,"direction_of_y_line_copies" ),
@@ -244,21 +263,21 @@ class OBJECT_PT_Duplicates_Panel(PanelMixin, bpy.types.Panel):
         for axis_name, visible, number_copies, distance_from_item, axis_path in set_panel:
         
             if visible:
-                column2.label(text = axis_name)
-                column2.prop(props, number_copies)
-                column2.prop(props, distance_from_item)
-                column2.prop(props, axis_path, expand=True)
-                
-            ### design your panel here ###
-   
+                column1.label(text = axis_name)
+                column1.prop(props, number_copies)
+                column1.prop(props, distance_from_item)
+                column1.prop(props, axis_path, expand=True)
+            
         ###axis toggles
         row1 = layout.row(align=True)
         row1.operator(OBJECT_OT_Toggle_X_Mode.bl_idname, text="X", depress=props.toggle_x_axis)
         row1.operator(OBJECT_OT_Toggle_Y_Mode.bl_idname, text="Y", depress=props.toggle_y_axis)
         row1.operator(OBJECT_OT_Toggle_Z_Mode.bl_idname, text="Z", depress=props.toggle_z_axis)
-        
+
+        ##buttons
         row2 = layout.row()
-        row2.operator(OBJECT_OT_Duplicate_All.bl_idname, text="Fire!")
+        row2.operator(OBJECT_OT_Duplicate_All.bl_idname, text="Fire")
+        row2.operator(OBJECT_OT_Reset_All.bl_idname, text="Reset")
 
 ###PANELS END
 
@@ -269,6 +288,7 @@ classes = [
     DuplicateObjectProperties,
     OBJECT_PT_Duplicates_Panel,
     OBJECT_OT_Duplicate_All,
+    OBJECT_OT_Reset_All,
     OBJECT_OT_Toggle_X_Mode,
     OBJECT_OT_Toggle_Y_Mode,
     OBJECT_OT_Toggle_Z_Mode,
@@ -286,4 +306,5 @@ def unregister():
     del bpy.types.Scene.dupe_props
     
 if __name__ == "__main__":
+
     register()
