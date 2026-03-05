5f72a22cfae4daabe4713ce1681f4ea001e5eaab
Lancine Doumbia <107377652+ldoum@users.noreply.github.com>
2026-03-04 23:40:31 -0500

    Enhance duplication logic and update parameters
    Updated the formula function to handle linked object duplication and non mesh objects

--- core_functionality.py
+++ core_functionality.py
@@ -11,47 +11,62 @@ def change_sign(pos_or_neg):
     else:
         return -1
 
-def formula(n, distance, axis, negate, collection_n):
+#duplicated objects remain active for multi axes copying
+def formula(n, distance, axis, negate, collection_n, linked_obj):
         
     for sing_obj in bpy.context.selected_objects:
             
         for i in range(n):
             
+            #make new physical object
             newbie = sing_obj.copy()
-            newbie.data = sing_obj.data.copy()
+            
+            #handles objects w/o edit data
+            if sing_obj.type not in ("EMPTY", "SPEAKER", "LIGHT"):
+                
+                # automate linked duplication. transfer data
+                if linked_obj:
+                    newbie.data = sing_obj.data
+                else:
+                    newbie.data = sing_obj.data.copy()
+                
+            #put object in new collectin    
             collection_n.objects.link(newbie)
             
+            #move the new object
             newbie.location[axis] = sing_obj.location[axis] + (i+1) * distance * change_sign(negate)
-
-
-
-
-n_x = bpy.data.collections.new("New Collection")
-bpy.context.scene.collection.children.link(n_x)
+            
 
 #inputs
 copies_x = 3
 distance_x = 5.00
 direction_x = 1
 
-copies_y = 2
-distance_y = 7.00
+copies_y = 10
+distance_y = 40.0
 direction_y = 1
 
 copies_z = 9
 distance_z = 9.00
 direction_z = 1
 
-toggle_x = True if bool(1) else False
+toggle_x = True if bool(0) else False
 toggle_y = True if bool(1) else False
-toggle_z = True if bool(1) else False 
+toggle_z = True if bool(0) else False 
+
+
+linked = True
+
+n_x = bpy.data.collections.new("New Collection")
+bpy.context.scene.collection.children.link(n_x)
+
 
 if toggle_x:
-    formula(3, 5.00, 0, 'RIGHT' if bool(direction_x) else 'LEFT', n_x)
+    formula(copies_x, distance_x, 0, 'RIGHT' if bool(direction_x) else 'LEFT', n_x, linked)
             
 if toggle_y:
-    formula(2, 7.00, 1, 'FORWARD' if bool(direction_y) else 'BACKWARD', n_x)
+    formula(copies_y, distance_y, 1, 'FORWARD' if bool(direction_y) else 'BACKWARD', n_x, linked)
             
 if toggle_z:
-    formula(9, 9.00, 2, 'UP' if bool(direction_z) else 'DOWN', n_x)      
+    formula(copies_z, distance_z, 2, 'UP' if bool(direction_z) else 'DOWN', n_x, linked)      
         
